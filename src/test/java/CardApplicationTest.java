37d0aa5e317963f84eb6a39467079d8fc3592d82
Taisia Ivanovskaya <taya.naumova@gmail.com>
2022-11-21 17:46:35 +0300

    Test added

--- src/test/java/CardApplicationTest.java
+++ src/test/java/CardApplicationTest.java
@@ -10,10 +10,19 @@ public class CardApplicationTest {
     void shouldSubmitRequest() {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
-        form.$("[data-test-id=name] input").setValue("Васильев Василий");
-        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=name] input").setValue("Иванов Иван");
+        form.$("[data-test-id=phone] input").setValue("+79210000000");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
         $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+    @Test
+    void shouldRequireAllFields() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Иванов Иван");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $(".input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 }
